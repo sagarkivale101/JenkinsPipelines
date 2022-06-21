aa135acccf4d90840d9fffe04922e1c82336e492
sagar.kivle@iauro.com <sagar.kivle@iauro.com>
2022-06-21 18:25:19 +0530

    EKS terraform Script

---
+++ pipe.groovy
@@ -0,0 +1,62 @@
+// pipeline steps
+// 1. pull terraform script from git
+// 2. Install aws CLI and aws configure with credentials.
+// 3. Install terraform and terraform init, plan, apply
+
+
+pipeline{
+    agent any
+    
+    stages{
+        stage('Checkout'){
+            steps {
+                    git url: 'https://github.com/sagarkivale101/terraformEKS.git', branch : "master"
+                    sh 'ls'
+                    //cd 'eks-terraform'
+                    //sh 'ls'
+            }
+        }
+        
+        stage('aws config'){
+ 
+            steps{
+                  sh "rm -rf awscliv2.zip"
+                  sh "rm -rf aws"
+                  sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
+                  sh "unzip awscliv2.zip"
+                  sh 'apt install sudo'
+               //   sh "sudo ./aws/install"
+                  sh "aws --version"
+                  sh "aws configure set aws_access_key_id 'AKIAVasdsamdbsajdjdkjas' && aws configure set aws_secret_access_key 'VkNbOXKMUasdvadsavdhgasdahdsFp5OIagsjaasd' && aws configure set region 'us-east-1'"
+                
+            }
+        }
+        stage('terraform apply'){
+    steps {
+        
+    sh "ls"
+           
+        sh 'apt-get update'
+        sh 'apt install sudo'
+        sh 'apt-get install wget'
+        sh "sudo apt install wget zip python3-pip -y"
+        sh "curl -o terraform.zip https://releases.hashicorp.com/terraform/1.2.3/terraform_1.2.3_linux_amd64.zip"
+        sh "unzip terraform.zip"
+        sh "sudo mv terraform /usr/bin"
+        sh "rm -rf terraform.zip"
+        
+        sh 'terraform version'
+        sh 'terraform init -no-color'
+        sh 'terraform plan -no-color'
+        // sh 'terraform apply -auto-approve'
+         sh 'terraform destroy -auto-approve'
+        
+    
+
+    }
+}
+
+
+
+}
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+pipeline steps
+1. pull terraform script from git
+2. Install aws CLI and aws configure with credentials.
+3. Install terraform and terraform init, plan, apply
